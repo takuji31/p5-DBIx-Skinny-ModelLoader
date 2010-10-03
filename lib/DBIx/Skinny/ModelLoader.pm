a57a166d74e48c0b90e55dc3724d6a90605556ec
Nishibayashi Takuji <takuji@senchan.jp>
2010-10-03 13:00:36 +0900

    initial commit

---
+++ lib/DBIx/Skinny/ModelLoader.pm
@@ -0,0 +1,32 @@
+package DBIx::Skinny::ModelLoader;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+DBIx::Skinny::ModelLoader -
+
+=head1 SYNOPSIS
+
+  use DBIx::Skinny::ModelLoader;
+
+=head1 DESCRIPTION
+
+DBIx::Skinny::ModelLoader is
+
+=head1 AUTHOR
+
+Nishibayashi Takuji E<lt>takuji {at} senchan.jpE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
